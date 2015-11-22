dc046ec6b24e2c95c1d61d67bc9a622f2a301475
hoony <thechunsik@gmail.com>
2015-11-22 18:09:01 +0900

    refactor a little

--- index.js
+++ index.js
@@ -6,20 +6,17 @@ module.exports = function(opts, callback) {
   var callback = callback || false;
   var opts = opts || {};
   var reqOpts = {};
-  reqOpts.encoding = 'binary';
 
   opts.encoding = opts.encoding || false;
   opts.blocked = opts.blocked || false;
 
-  if (!opts.url) {
-    throw new Error('Must supply url');
-  } else {
-    reqOpts.url = opts.url;
-  }
-
+  if (!opts.url) throw new Error('Must supply url');
   // cheatting the server
   if (opts.blocked) reqOpts.headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:43.0) Gecko/20100101 Firefox/43.0'};
   if (opts.encoding && (!opts.sourceEncoding || !opts.sourceEncoding)) throw new Error('Must specify source & target encoding');
+
+  reqOpts.url = opts.url;
+  reqOpts.encoding = 'binary';
 
   request(reqOpts, function (err, res, html) {
     if (opts.encoding) {
